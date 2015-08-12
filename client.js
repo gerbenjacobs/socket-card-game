72794c3e6ba911007eaca3ded18e55e724022809
gerbenjacobs <jacobs.gerben@gmail.com>
2015-08-13 00:12:19 +0200

    All these things I did in the evening. #bestcommitmessageever

--- client.js
+++ client.js
@@ -3,7 +3,11 @@ $(document).ready(function(){
     var socket = io.connect(location.hostname + ':3000');
     var lobbyList = $("#lobby-list");
     var lobbyCount = $("#lobby-count");
+    var notify = $("#notify");
     var playgroundTitle = $("#playground-title");
+    var playgroundInvitee = $("#playground-opponent");
+    var playgroundCards = $("#playground-cards");
+    var me;
 
     $(document).on( "click", ".invite-link", function(){
         var player = $(this).parent().attr("id");
@@ -11,14 +15,31 @@ $(document).ready(function(){
         return false;
     });
 
+    $("#debug-button").click(function(){
+        socket.emit("debug");
+    });
+    socket.on('debug', function(games, users){
+        var debug = "<pre>";
+        debug += JSON.stringify(games, null, 2);
+        debug += "\n\n";
+        debug += JSON.stringify(users, null, 2);
+        $("#debug").html(debug);
+    });
+
     socket.on('connect',function(){
         console.log("Connected to the sever")
     });
 
+    socket.on('notify', function(msg){
+        notifyUser(msg);
+    });
+
     socket.on("welcome", function(user, users) {
        $("#welcome").html("Welcome, you're now known as <strong>"+user.username+"</strong>");
+        me = user;
         updateUsers(users);
     });
+
     socket.on('user_joined', function(user) {
         if (user.socket != socket.id) {
             lobbyCount.text(Number(lobbyCount.text()) + 1);
@@ -31,17 +52,28 @@ $(document).ready(function(){
         $("#" + socket).remove();
     });
 
+    socket.on('game_crash', function() {
+        console.log("Game crash");
+        notifyUser("Your opponent left. I'm sorry!");
+        playgroundTitle.empty();
+        playgroundCards.empty();
+        playgroundInvitee.empty();
+    });
+
     socket.on('receive_invite', function(game, player) {
         console.log("Received invite from", player, "for", game);
         if (confirm(player.username + " wants to play a game with you! Do you accept?")) {
             socket.emit("accept_invite", player.socket, game.id);
             startGame(game, player);
+            console.log("i got invited and accepted");
+
         } else {
             socket.emit("decline_invite", player.socket, game.id);
         }
     });
 
     socket.on('invite_accepted', function(game, player){
+        console.log("invite_accepted");
         startGame(game, player);
     });
 
@@ -62,8 +94,19 @@ $(document).ready(function(){
     }
 
     function startGame(game, player) {
-        console.log(game);
         console.log(player);
-        playgroundTitle.html(game.id);
+        console.log(me);
+        playgroundTitle.html("Game: " + game.id);
+        playgroundInvitee.html("<img src='https://robohash.org/"+player.username+"' alt='"+player.username+"' title='"+player.username+"' width='100'><br>" + player.username);
+        playgroundCards.html("<img src='https://robohash.org/"+me.username+"' alt='"+me.username+"' title='"+me.username+"' width='100'><br>" + me.username);
+    }
+
+    function notifyUser(msg) {
+        notify.html(msg).show();
+        setTimeout(function(){
+            notify.slideUp(800, function(){
+                $(this).empty();
+            });
+        }, 3000);
     }
 });
--- server.js
+++ server.js
@@ -7,47 +7,81 @@ var moniker = require('moniker');
 console.log("Server listening on localhost:" + port);
 
 var users = {};
-var games = {};
+var games = [];
 
 io.on('connection', function (socket) {
-    var user = {socket: socket.id, username: moniker.choose()};
+    var user = {socket: socket.id, username: moniker.choose(), playing: false};
     users[socket.id] = user;
 
     socket.emit("welcome", user, users);
+    socket.emit("notify", "Testing the notify bar!");
     io.emit("user_joined", user);
 
-    socket.on('disconnect', function() {
+    socket.on('debug', function () {
+        socket.emit("debug", games, users);
+    });
+
+    socket.on('disconnect', function () {
+        io.emit("user_left", socket.id);
+
+        for (var key in games) {
+            console.log(games);
+            if (games.hasOwnProperty(key)) {
+                var game = games[key];
+                if (game) {
+                    if (game.player.socket == socket.id) {
+                        io.to(game.invitee.socket).emit("game_crash");
+                        users[game.invitee.socket].playing = false;
+                    } else if (game.invitee.socket == socket.id) {
+                        io.to(game.player.socket).emit("game_crash");
+                        users[game.player.socket].playing = false;
+                    }
+                    delete games[game];
+                }
+            }
+        }
+
         delete users[socket.id];
-        io.emit("user_left", socket.id)
     });
 
-    socket.on('send_invite', function(player) {
-        var game_id = moniker.choose();
-        var game = {
-            id: game_id,
-            player: users[socket.id],
-            invitee: users[player],
-            active: false,
-            state: "invited"
-        };
-        games[game_id] = game;
-        io.sockets.connected[player].emit("receive_invite", game, users[socket.id]);
+    socket.on('send_invite', function (player) {
+        if (!users[player].playing) {
+            var game_id = moniker.choose();
+            var game = {
+                id: game_id,
+                player: users[socket.id],
+                invitee: users[player],
+                active: false,
+                state: "invited"
+            };
+            games[game_id] = game;
+            io.to(player).emit("receive_invite", game, users[socket.id]);
+        } else {
+            socket.emit('notify', 'This user is already playing.');
+        }
     });
 
-    socket.on('accept_invite', function(player_id, game_id){
+    socket.on('accept_invite', function (player_id, game_id) {
         var game = games[game_id];
         var player = users[player_id];
+
+        // Make game active
         game.state = "playing";
         game.active = true;
 
+        // Make players active
+        player.playing = true;
+        users[socket.id].playing = true;
+
         // Send to sender
-        io.sockets.connected[player_id].emit("invite_accepted", game, player);
+        io.to(player_id).emit("invite_accepted", game, game.invitee);
 
     });
-    socket.on('accept_decline', function(player_id, game_id){
-        var game = games[game_id];
-        game.state = "declined";
-        game.active = false;
+    socket.on('decline_invite', function (player_id, game_id) {
+        delete games[game_id];
+
+        // Send to sender
+        io.to(player_id).emit("notify", "The other player declined your invite.");
 
     });
 
